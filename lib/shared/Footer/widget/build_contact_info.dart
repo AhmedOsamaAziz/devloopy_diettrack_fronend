a88d05a5a138d73f1254437e79e649b18113f3d7
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-03-23 21:02:15 +0200

    Refactor footer layouts and navigation
    -   Update footer structure to use separate layouts for desktop, tablet, and mobile views.
    -   Refactor navigation buttons to be reusable across footer layouts.
    -   Update contact info to use responsive font sizes.
    -   Remove unused code and comments.
    -   Update build nav button footer widget to NavButton.
    -   Update mobile navigation bar to wrap and use NavButton.
    -   Update Tablet footer.
    -   Update desktop footer.

--- lib/shared/Footer/widget/build_contact_info.dart
+++ lib/shared/Footer/widget/build_contact_info.dart
@@ -1,56 +1,62 @@
-// import 'package:flutter/material.dart';
-// import 'package:ui/constants/constants.dart';
-// import 'package:ui/helper/screen_size.dart';
-//
-// class BuildContactInfo extends StatelessWidget {
-//   const BuildContactInfo({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ScreenSize.isLarge
-//         ? Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             spacing: 15,
-//             children: [
-//               _buildContactItem(Icons.mail, "Ahmed.Madkour@gmail.com"),
-//               _buildContactItem(Icons.phone, "+002000020200"),
-//               _buildContactItem(Icons.location_on, "SomeWhere in the World"),
-//               _buildContactItem(
-//                   Icons.copyright, "2024 DevLoopy.com.All rights reserved"),
-//             ],
-//           )
-//         : Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             spacing: 15,
-//             children: [
-//               _buildContactItem(Icons.mail, "Ahmed.Madkour@gmail.com"),
-//               _buildContactItem(Icons.phone, "+002000020200"),
-//               _buildContactItem(Icons.location_on, "SomeWhere in the World"),
-//               _buildContactItem(
-//                   Icons.copyright, "2024 DevLoopy.com.All rights reserved"),
-//             ],
-//           );
-//   }
-//
-//   Widget _buildContactItem(IconData icon, String text) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 5),
-//       child: Container(
-//         width: 300,
-//         height: 30,
-//         decoration: BoxDecoration(
-//             color: ColorsApp.AppBarColor,
-//             borderRadius: BorderRadius.circular(5)),
-//         child: Row(
-//           mainAxisSize: MainAxisSize.max,
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Icon(icon, color: Colors.white, size: 18),
-//             const SizedBox(width: 5),
-//             Text(text, style: const TextStyle(color: Colors.white)),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:ui/constants/constants.dart';
+import 'package:ui/helper/font_size_responsive.dart';
+import 'package:ui/helper/screen_size.dart';
+import 'package:ui/shared/custom_text.dart';
+
+class BuildContactInfo extends StatelessWidget {
+  const BuildContactInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenSize.isLarge
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 15,
+            children: [
+              _buildContactItem(Icons.mail, "Ahmed.Madkour@gmail.com", context),
+              _buildContactItem(Icons.phone, "+002000020200", context),
+              _buildContactItem(
+                  Icons.location_on, "SomeWhere in the World", context),
+              _buildContactItem(Icons.copyright,
+                  "2024 DevLoopy.com.All rights reserved", context),
+            ],
+          )
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 15,
+            children: [
+              _buildContactItem(Icons.mail, "Ahmed.Madkour@gmail.com", context),
+              _buildContactItem(Icons.phone, "+002000020200", context),
+              _buildContactItem(
+                  Icons.location_on, "SomeWhere in the World", context),
+              _buildContactItem(Icons.copyright,
+                  "2024 DevLoopy.com.All rights reserved", context),
+            ],
+          );
+  }
+}
+
+Widget _buildContactItem(IconData icon, String text, BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 5),
+    child: Container(
+      width: 300,
+      height: 30,
+      decoration: BoxDecoration(
+          color: ColorsApp.AppBarColor, borderRadius: BorderRadius.circular(5)),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: Colors.white, size: 18),
+          const SizedBox(width: 5),
+          CustomText(
+              text: text,
+              color: Colors.white,
+              fontSize: getResponsiveFontSize(context, fontSize: 12)),
+        ],
+      ),
+    ),
+  );
+}
