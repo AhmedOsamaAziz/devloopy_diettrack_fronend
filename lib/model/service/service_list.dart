bc5b8921db7ec9432b66cdc9e299a32e2db15db2
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-03-25 21:41:25 +0200

    Refactor service management: update service model to include items, enhance dashboard functionality, and clean up pubspec.yaml

--- lib/model/service/service_list.dart
+++ lib/model/service/service_list.dart
@@ -1,10 +1,13 @@
-import 'service_base.dart';
+import 'package:ui/model/service/service_base.dart';
+import 'package:ui/model/service_item/service_item_details.dart';
 
 class ServiceList extends ServiceBase {
   int? id;
+  List<ServiceItemDetails>? items;
 
   ServiceList({
     this.id,
+    this.items,
     required super.name,
     required super.nameAr,
     required super.price,
@@ -18,22 +21,25 @@ class ServiceList extends ServiceBase {
   factory ServiceList.fromJson(Map<String, dynamic> json) {
     return ServiceList(
       id: json['id'],
-      name: json['name'] ,
-      nameAr: json['nameAr'] ,
+      name: json['name'],
+      nameAr: json['nameAr'],
       price: (json['price'] as num?)?.toDouble() ?? 0.0,
-      validFor: json['validFor']  ,
+      validFor: json['validFor'],
       description: json.containsKey('items') && json['items'] is List
           ? (json['items'] as List<dynamic>)
               .map((item) => item['description'] as String? ?? '')
               .join('; ')
-          : 'No description available',
-      descriptionAr:
-          json['descriptionAr'] as String? ?? 'Default Description Arabic',
+          : json['description'] ?? '',
+      descriptionAr: json['descriptionAr'] ?? '',
       isBestValue: json['isBestValue'] as bool? ?? false,
       discount: (json['discount'] as num?)?.toInt(),
+      items: (json['items'] as List<dynamic>?)
+          ?.map((item) => ServiceItemDetails.fromJson(item))
+          .toList(),
     );
   }
 
+ 
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
@@ -44,5 +50,6 @@ class ServiceList extends ServiceBase {
         'descriptionAr': descriptionAr,
         'isBestValue': isBestValue,
         'discount': discount,
+        'items': items?.map((item) => item.toJson()).toList(),
       };
 }
--- lib/screens/dashboard_screen/screens/service/add_service.dart
+++ lib/screens/dashboard_screen/screens/service/add_service.dart
@@ -1,73 +1,112 @@
+// add_service.dart
 import 'package:flutter/material.dart';
 
-class AddServiceDialog extends StatelessWidget {
-  final Function(Map<String, String>) onSave;
+class AddService extends StatefulWidget {
+  final Function(Map<String, dynamic>) onSave;
 
-  const AddServiceDialog({required this.onSave, super.key});
+  const AddService({required this.onSave, super.key});
+
+  @override
+  _AddServiceState createState() => _AddServiceState();
+}
+
+class _AddServiceState extends State<AddService> {
+  final _formKey = GlobalKey<FormState>();
+  final List<Map<String, TextEditingController>> _items = [];
+  final nameController = TextEditingController();
+  final nameArController = TextEditingController();
+  final priceController = TextEditingController();
+  final validForController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final descriptionArController = TextEditingController();
+  String isBestValue = 'false';
+
+  @override
+  void initState() {
+    super.initState();
+    _addItem();
+  }
+
+  void _addItem() {
+    setState(() {
+      _items.add({
+        'description': TextEditingController(),
+        'descriptionAr': TextEditingController(),
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final nameController = TextEditingController();
-    final nameArController = TextEditingController();
-    final priceController = TextEditingController();
-    final validForController = TextEditingController();
-    final isBestValueController = TextEditingController();
-    final discountController = TextEditingController();
-    final descriptionController = TextEditingController();
-    final descriptionArController = TextEditingController();
-    String isBestValue = 'false';
     return AlertDialog(
       title: const Text('Add New Service'),
       content: SingleChildScrollView(
-        child: SizedBox(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              TextField(
-                controller: nameArController,
-                decoration: const InputDecoration(labelText: 'Name Ar'),
-              ),
-              TextField(
-                controller: priceController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Price'),
-              ),
-              TextField(
-                controller: validForController,
-                decoration: const InputDecoration(labelText: 'Valid For'),
-              ),
-
-              DropdownButtonFormField<String>(
-                value: isBestValue,
-                decoration: const InputDecoration(labelText: 'Is Best Value'),
-                items: ['true', 'false'].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
+        child: Form(
+          key: _formKey,
+          child: SizedBox(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
+                TextField(
+                  controller: nameArController,
+                  decoration: const InputDecoration(labelText: 'Name Ar'),
+                ),
+                TextField(
+                  controller: priceController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Price'),
+                ),
+                TextField(
+                  controller: validForController,
+                  decoration: const InputDecoration(labelText: 'Valid For'),
+                ),
+                DropdownButtonFormField<String>(
+                  value: isBestValue,
+                  decoration: const InputDecoration(labelText: 'Is Best Value'),
+                  items: ['true', 'false'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      isBestValue = newValue!;
+                    });
+                  },
+                ),
+                TextField(
+                  controller: descriptionController,
+                  decoration: const InputDecoration(labelText: 'Description'),
+                ),
+                TextField(
+                  controller: descriptionArController,
+                  decoration:
+                      const InputDecoration(labelText: 'Description Ar'),
+                ),
+                ..._items.asMap().entries.map((entry) {
+                  final controllers = entry.value;
+                  return Column(
+                    children: [
+                      TextField(
+                        controller: controllers['description'],
+                        decoration: const InputDecoration(
+                            labelText: 'Item Description'),
+                      ),
+                      TextField(
+                        controller: controllers['descriptionAr'],
+                        decoration: const InputDecoration(
+                            labelText: 'Item Description Ar'),
+                      ),
+                    ],
                   );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  isBestValue = newValue!;
-                },
-              ),
-              // TextField(
-              //   controller: discountController,
-              //   keyboardType: TextInputType.number,
-              //   decoration: const InputDecoration(labelText: 'Discount'),
-              // ),
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(labelText: 'Description'),
-              ),
-              TextField(
-                controller: descriptionArController,
-                decoration: const InputDecoration(labelText: 'Description Ar'),
-              ),
-            ],
+                }),
+              ],
+            ),
           ),
         ),
       ),
@@ -78,15 +117,22 @@ class AddServiceDialog extends StatelessWidget {
         ),
         TextButton(
           onPressed: () {
+            final itemsData = _items
+                .map((item) => {
+                      'description': item['description']!.text,
+                      'descriptionAr': item['descriptionAr']!.text,
+                    })
+                .toList();
+
             Navigator.of(context).pop({
               'name': nameController.text,
               'nameAr': nameArController.text,
               'price': priceController.text,
               'validFor': validForController.text,
-              'isBestValue': isBestValueController.text,
-              // 'discount': discountController.text,
+              'isBestValue': isBestValue,
               'description': descriptionController.text,
               'descriptionAr': descriptionArController.text,
+              'items': itemsData,
             });
           },
           child: const Text('Save'),
